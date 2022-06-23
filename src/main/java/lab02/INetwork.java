d25c72351539f82f32c600478a0d116831f824a7
Аліса Лахтюк <alisalahtiuk@gmail.com>
2022-06-23 22:10:42 +0300

    tic-tac-toi

--- src/main/java/lab02/INetwork.java
+++
@@ -1,8 +0,0 @@
-package lab02;
-
-import java.net.InetAddress;
-
-public interface INetwork {
-    void receiveMessage();
-    void sendMessage(byte[] mess, InetAddress inetAddress) throws Exception;
-}
